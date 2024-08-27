e5238544b64d55764fad00a37ec5f8a89fb05bca
Murilo Thom <murilothom.2002@hotmail.com>
2024-08-26 21:29:58 -0300

    Create user and seed database

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbilityFor, projectSchema } from '@repo/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER', id: '123' })
-
-const project = projectSchema.parse({ id: '321', ownerId: '123' })
-
-console.log(ability.can('get', 'Billing'))
-console.log(ability.can('create', 'Invite'))
-console.log(ability.can('delete', project))
